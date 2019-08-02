f32bbac99b553cd24a4c748972a1efb2b6cc134f
ninetyfivejae <jh33836782@gmail.com>
2019-08-03 00:56:43 +0900

    Feat: API 호출 시 파라미터 넘겨주는 방식도 사용가능하게 변경

--- KakaopayWeatherApp/KakaopayWeatherApp/Networking/HTTPHelper.swift
+++ KakaopayWeatherApp/KakaopayWeatherApp/Networking/HTTPHelper.swift
@@ -6,7 +6,7 @@ class API: HttpHelper {
 	
 	func requestWeather(latitude: Double, longitude: Double, resultHandler: @escaping (Any) -> Void) {
 		let url = URL(string: "\(URLs.baseURL)/\(HttpHelper.accessToken)/\(latitude),\(longitude)")!
-		get(url: url, completionHandler: { data, response, error in
+		get(url: url, parameters: ["units": "si"], completionHandler: { data, response, error in
 			guard error == nil else { return }
 			if	let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
 				guard let jsonToArray = try? JSONSerialization.jsonObject(with: data, options: []) else { return }
@@ -37,51 +37,61 @@ class HttpHelper {
 		case post = "POST"
 		case get = "GET"
 		case put = "PUT"
-		case patch = "PATCH"
 		case delete = "DELETE"
 	}
 	
 	let session: URLSession = URLSession.shared
 	
-	func get(url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+	func get(url: URL, parameters: [String: Any]? = nil, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
 		var request: URLRequest = URLRequest(url: url)
 		request.httpMethod = HTTPMethod.get.rawValue
 		request.addValue("application/json", forHTTPHeaderField: "Accept")
+		if let parameters = parameters {
+			request.url = URL(string:"\(url)\(getParameterString(parameters: parameters))")
+		}
 		session.dataTask(with: request, completionHandler: completionHandler).resume()
 	}
 	
-	func post(url: URL, body: NSMutableDictionary, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) throws {
+	func post(url: URL, parameters: [String: Any]? = nil, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) throws {
 		var request: URLRequest = URLRequest(url: url)
 		request.httpMethod = HTTPMethod.post.rawValue
 		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 		request.addValue("application/json", forHTTPHeaderField: "Accept")
-		request.httpBody = try JSONSerialization.data(withJSONObject: body, options: JSONSerialization.WritingOptions.prettyPrinted)
+		if let parameters = parameters {
+			request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+		}
 		session.dataTask(with: request, completionHandler: completionHandler).resume()
 	}
 	
-	func put(url: URL, body: NSMutableDictionary, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) throws {
+	func put(url: URL, parameters: [String: Any]? = nil, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) throws {
 		var request: URLRequest = URLRequest(url: url)
 		request.httpMethod = HTTPMethod.put.rawValue
 		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 		request.addValue("application/json", forHTTPHeaderField: "Accept")
-		request.httpBody = try JSONSerialization.data(withJSONObject: body, options: JSONSerialization.WritingOptions.prettyPrinted)
+		if let parameters = parameters {
+			request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+		}
 		session.dataTask(with: request, completionHandler: completionHandler).resume()
 	}
 	
-	func patch(url: URL, body: NSMutableDictionary, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) throws {
-		var request: URLRequest = URLRequest(url: url)
-		request.httpMethod = HTTPMethod.patch.rawValue
-		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-		request.addValue("application/json", forHTTPHeaderField: "Accept")
-		request.httpBody = try JSONSerialization.data(withJSONObject: body, options: JSONSerialization.WritingOptions.prettyPrinted)
-		session.dataTask(with: request, completionHandler: completionHandler).resume()
-	}
-	
-	func delete(url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+	func delete(url: URL, parameters: [String: Any]? = nil, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
 		var request: URLRequest = URLRequest(url: url)
 		request.httpMethod = HTTPMethod.delete.rawValue
 		request.addValue("application/json", forHTTPHeaderField: "Accept")
 		session.dataTask(with: request, completionHandler: completionHandler).resume()
+	}
+	
+	func getParameterString(parameters: [String: Any]? = nil) -> String {
+		guard let parameters = parameters else { return "" }
+		var urlValues = [String]()
+		parameters.forEach { (key: String, value: Any) in
+			guard let value = value as? String else { return }
+			if let encodedValue = value.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) {
+				urlValues.append(key + "=" + encodedValue)
+			}
+		}
+		let firstValue = urlValues.removeFirst()
+		return urlValues.reduce("?\(firstValue)") { return $0 + "&" + $1 }
 	}
 	
 }
