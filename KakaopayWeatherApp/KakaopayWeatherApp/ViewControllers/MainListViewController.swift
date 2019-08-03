d621a82d04f7210cb6060f86c372af517e74e43c
ninetyfivejae <jh33836782@gmail.com>
2019-08-03 12:49:47 +0900

    Feat: 메인 리스트 화면 오른쪽 상단 편집 버튼 추가
    - 누르면 편집 모드로 변경
    - 다시 누르면 복귀

--- KakaopayWeatherApp/KakaopayWeatherApp/ViewControllers/MainListViewController.swift
+++ KakaopayWeatherApp/KakaopayWeatherApp/ViewControllers/MainListViewController.swift
@@ -5,6 +5,7 @@ class MainListViewController: UIViewController {
 	var viewModel: ViewModel?
 	var vcList: [String] = []
 	
+	var rightEditButton = UIBarButtonItem()
 	@IBOutlet weak var tableView: UITableView!
 	
 	override func viewDidLoad() {
@@ -13,10 +14,32 @@ class MainListViewController: UIViewController {
 		tableView.delegate = self
 		tableView.dataSource = self
 		
+		setupRightEditButton()
+		
 		guard let viewModel = viewModel else { return }
 		addObservers(to: viewModel)
 		
 		viewModel.requestWeather()
+	}
+	
+	func setupRightEditButton() {
+		rightEditButton = UIBarButtonItem(title: "편집", style: .plain, target: self, action: #selector(editButtonAction))
+		self.navigationItem.rightBarButtonItem = rightEditButton
+		self.navigationItem.rightBarButtonItem?.tintColor = UIColor.darkGray
+		//rightEditButton.isEnabled = !remocon.remoconReservations.isEmpty
+	}
+	
+	@objc func editButtonAction() {
+		setEditing(!tableView.isEditing, animated: true)
+		if !tableView.isEditing {
+			//requestList()
+		}
+		rightEditButton.title = tableView.isEditing ? "완료" : "편집"
+	}
+	
+	override func setEditing(_ editing: Bool, animated: Bool) {
+		super.setEditing(editing, animated: animated)
+		self.tableView.setEditing(editing, animated: animated)
 	}
 	
 	func addObservers(to viewModel: ViewModel) {
@@ -39,11 +62,24 @@ extension MainListViewController: UITableViewDelegate, UITableViewDataSource {
 	}
 	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
 		self.show(DetailWeatherViewController.instance, sender: self)
 	}
 	
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
+			tableView.deleteRows(at: [indexPath], with: .automatic)
+		}
+	}
+	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		return UITableViewCell()
+		return configureCityCell(tableView: tableView, indexPath: indexPath)
+	}
+	
+	func configureCityCell(tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: CityCell.className, for: indexPath) as? CityCell else { return UITableViewCell() }
+		cell.accessoryType = .disclosureIndicator
+		return cell
 	}
 	
 }
