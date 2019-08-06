dbe6d4aaa1e160f4603f605011d4e3240edab96a
ninetyfivejae <jh33836782@gmail.com>
2019-08-06 13:09:48 +0900

    Feat: 위치 검색 결과 요청하는 방식 변경

--- KakaopayWeatherApp/KakaopayWeatherApp/ViewControllers/SearchMapViewController.swift
+++ KakaopayWeatherApp/KakaopayWeatherApp/ViewControllers/SearchMapViewController.swift
@@ -10,7 +10,6 @@ class SearchMapViewController: UIViewController, ObserverProtocol {
 	private var search: MKLocalSearch? =  nil
 	private var searchCompleter = MKLocalSearchCompleter()
 	private var places = [MKLocalSearchCompletion]()
-	//var cityList: [MKMapItem] = []
 
 	@IBOutlet weak var tableView: UITableView!
 	@IBOutlet weak var searchBar: UISearchBar!
@@ -24,21 +23,10 @@ class SearchMapViewController: UIViewController, ObserverProtocol {
 		
 		self.manager.delegate = self
 		self.searchCompleter.delegate = self
-		
-		if CLLocationManager.locationServicesEnabled() {
-			manager.requestWhenInUseAuthorization()
-		}
 	}
 	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-	}
-	
-	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-		super.touchesBegan(touches, with: event)
-		
-		self.tableView.resignFirstResponder()
-		self.searchBar.resignFirstResponder()
 	}
 
 	@IBAction func dismiss(_ sender: Any) {
@@ -58,24 +46,17 @@ extension SearchMapViewController: UITableViewDelegate, UITableViewDataSource {
 		return places.count
 	}
 	
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		return configureSearchedCityCell(tableView: tableView, indexPath: indexPath)
-	}
-	
-	func configureSearchedCityCell(tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
-		guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchedCityCell.className, for: indexPath) as? SearchedCityCell else { return UITableViewCell() }
-		cell.cityNameLabel.text = places[indexPath.item].title
-		cell.addressLabel.text = places[indexPath.item].subtitle
-		return cell
-	}
-	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		let request = MKLocalSearch.Request()
 		request.naturalLanguageQuery = places[indexPath.item].title
 		let search = MKLocalSearch(request: request)
 		search.start { (response, error) in
-			guard let response = response else { return }
-			guard let item = response.mapItems.first else { return }
+			
+			guard
+				error == nil,
+				let response = response,
+				response.mapItems.count > 0,
+				let item = response.mapItems.first else { return }
 			
 			var savedCityList = UserDefaults.standard.array(forKey: "CityList") as? [String] ?? []
 			if let selectedCity = item.placemark.locality {
@@ -90,6 +71,17 @@ extension SearchMapViewController: UITableViewDelegate, UITableViewDataSource {
 			tableView.deselectRow(at: indexPath, animated: true)
 			self.dismiss(self)
 		}
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return configureSearchedCityCell(tableView: tableView, indexPath: indexPath)
+	}
+	
+	func configureSearchedCityCell(tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchedCityCell.className, for: indexPath) as? SearchedCityCell else { return UITableViewCell() }
+		cell.cityNameLabel.text = places[indexPath.item].title
+		cell.addressLabel.text = places[indexPath.item].subtitle
+		return cell
 	}
 	
 }
@@ -115,6 +107,7 @@ extension SearchMapViewController: UISearchBarDelegate {
 	
 	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
 		searchCompleter.queryFragment = searchText
+		searchCompleter.filterType = .locationsOnly
 	}
 	
 }
@@ -122,12 +115,7 @@ extension SearchMapViewController: UISearchBarDelegate {
 extension SearchMapViewController: CLLocationManagerDelegate {
 	
 	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-		switch  status {
-		case .authorizedAlways, .authorizedWhenInUse:
-			manager.requestLocation()
-		default:
-			break
-		}
+		manager.requestLocation()
 	}
 	
 	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -135,8 +123,7 @@ extension SearchMapViewController: CLLocationManagerDelegate {
 	}
 	
 	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-		guard let location = locations.first else { return }
-		searchCompleter.region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 150, longitudinalMeters: 150)
+
 	}
 	
 }
