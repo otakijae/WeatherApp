eae1453347f52b1e8d86f662f578eb14248677b6
ninetyfivejae <jh33836782@gmail.com>
2019-12-13 13:43:25 +0900

    Chore: 알림창 문구 변경

--- WeatherApp/WeatherApp/Utils/Constants.swift
+++ WeatherApp/WeatherApp/Utils/Constants.swift
@@ -3,7 +3,7 @@ struct Constants {
 		case cityList = "CityList"
 	}
 	
-	static let applicationName = "카카오페이 날씨"
+	static let applicationName = "날씨"
 	static let networkAlertMessage = "WIFI 혹은 4G/LTE\n연결 상태를 확인해주세요."
 	static let edit = "편집"
 	static let complete = "완료"
